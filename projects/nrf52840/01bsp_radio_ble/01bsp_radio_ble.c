fefe165f0e048b8858c5244ed36602e84c15db66
yiming yuan <yyuan861@connect.hkust-gz.edu.cn>
2024-04-13 21:08:46 +0800

    add nordic as a short ble receiver

--- projects/nrf52840/01bsp_radio_ble/01bsp_radio_ble.c
+++ projects/nrf52840/01bsp_radio_ble/01bsp_radio_ble.c
@@ -22,13 +22,20 @@ end of frame event), it will turn on its error LED.
 #include "leds.h"
 #include "sctimer.h"
 #include "radio_ble.h"
+#include "uart.h"
 
 //=========================== defines =========================================
 
 #define LENGTH_PACKET   125+LENGTH_CRC  ///< maximum length is 127 bytes
-#define CHANNEL         38              ///< 0~39
+#define CHANNEL         0              ///< 0~39
 #define TIMER_PERIOD    (0xffff>>2)     ///< 0xffff = 2s@32kHz
 #define TXPOWER         0xD5            ///< 2's complement format, 0xD8 = -40dbm
+#define LENGTH_SERIAL_FRAME  127              // length of the serial frame
+
+
+#define LEN_PKT_TO_SEND 20+LENGTH_CRC
+char stringToSend[]  = "YYMYYMYYM\n";
+uint16_t length = 0;
 
 const static uint8_t ble_device_addr[6] = { 
     0xaa, 0xbb, 0xcc, 0xcc, 0xbb, 0xaa
@@ -64,6 +71,11 @@ typedef struct {
 app_dbg_t app_dbg;
 
 typedef struct {
+     // uart
+                uint8_t    uart_txFrame[LENGTH_SERIAL_FRAME];
+                uint8_t    uart_lastTxByte;
+    volatile    uint8_t    uart_done;
+
                 uint8_t         flags;
                 app_state_t     state;
                 uint8_t         packet[LENGTH_PACKET];
@@ -81,7 +93,14 @@ void     cb_startFrame(PORT_TIMER_WIDTH timestamp);
 void     cb_endFrame(PORT_TIMER_WIDTH timestamp);
 void     cb_timer(void);
 
+// uart
+void cb_uartTxDone(void);
+uint8_t cb_uartRxCb(void);
+void send_string(const char* str);
+
+
 void     assemble_ibeacon_packet(void);
+void     assemble_test_packet();
 
 //=========================== main ============================================
 
@@ -89,7 +108,7 @@ void     assemble_ibeacon_packet(void);
 \brief The program starts executing here.
 */
 int mote_main(void) {
-    uint8_t i;
+    uint8_t i,j,m;
 
     uint8_t freq_offset;
     uint8_t sign;
@@ -108,6 +127,13 @@ int mote_main(void) {
 
     // prepare packet
     app_vars.packet_len = sizeof(app_vars.packet);
+
+    // setup UART
+    uart_setCallbacks(cb_uartTxDone,cb_uartRxCb);
+    uart_enableInterrupts();
+    length = strlen("uart is ok!");
+    send_string("uart is ok!");
+
 
     // start bsp timer
     sctimer_set_callback(cb_timer);
@@ -183,6 +209,53 @@ int mote_main(void) {
                             &app_vars.rxpk_crc
                         );
 
+                        if (app_vars.packet[0] == 0xAA) {
+                            leds_debug_toggle();
+                        }
+
+                        if (app_vars.packet[0] == 0x04) {
+                            leds_debug_toggle();
+                        }
+                        if (app_vars.packet_len <10) {
+                            leds_debug_toggle();
+                            m = 0;
+                            //stringToSend[m++] = 'P';
+                            //stringToSend[m++] = 'A';
+                            //stringToSend[m++] = 'C';
+                            //stringToSend[m++] = 'K';
+                            //stringToSend[m++] = ':';
+                            length = 5;
+                            send_string("PACK: ");
+                            //for(j = 0; j < app_vars.packet_len; ++j){
+                            //  //stringToSend[m++] = app_vars.packet[j];
+                            j = 2;
+                            stringToSend[m++] = '0' + app_vars.packet[j] / 10;
+                            stringToSend[m++] = '0' + app_vars.packet[j++] % 10;
+                            stringToSend[m++] = '.';
+                            stringToSend[m++] = '0' + app_vars.packet[j] / 10;
+                            stringToSend[m++] = '0' + app_vars.packet[j++] % 10;
+                            stringToSend[m++] = '.';
+                            stringToSend[m++] = '0' + app_vars.packet[j] / 10;
+                            stringToSend[m++] = '0' + app_vars.packet[j++] % 10;
+                            stringToSend[m++] = '\r';
+                            stringToSend[m++] = '\n';
+                              //send_string(app_vars.packet[j]);
+                            //}
+                            length = 10;
+                            send_string(stringToSend);
+                            //stringToSend[m++] = '\r';
+                            //stringToSend[m++] = '\n';
+                            //send_string("\r\n");
+                        }
+
+                        //// send string over UART
+                        //if (app_vars.uartDone == 1) {
+                        //    app_vars.uartDone              = 0;
+                        //    app_vars.uart_lastTxByteIndex  = 0;
+                        //    uart_writeByte(stringToSend[app_vars.uart_lastTxByteIndex]);
+                        //}
+
+
                         // led
                         leds_error_off();
 
@@ -220,6 +293,8 @@ int mote_main(void) {
                     app_vars.packet_len = sizeof(app_vars.packet);
                     
                     assemble_ibeacon_packet();
+                    //assemble_test_packet();
+
 
                     // start transmitting packet
                     radio_ble_loadPacket(app_vars.packet,LENGTH_PACKET);
@@ -269,6 +344,20 @@ void assemble_ibeacon_packet(void) {
     app_vars.packet[i++]  = TXPOWER;            // power level
 }
 
+void assemble_test_packet(void) {
+
+    uint8_t i;
+    i=0;
+
+    memset( app_vars.packet, 0x00, sizeof(app_vars.packet) );
+
+    app_vars.packet[i++]  = 0x20;               // BLE ADV_NONCONN_IND (this is a must)
+    app_vars.packet[i++]  = 0x03;               // Payload length
+    app_vars.packet[i++]  = 24; // BLE adv address byte 0
+    app_vars.packet[i++]  = 24; // BLE adv address byte 1
+    app_vars.packet[i++]  = 24; // BLE adv address byte 2
+}
+
 //=========================== callbacks =======================================
 
 void cb_startFrame(PORT_TIMER_WIDTH timestamp) {
@@ -295,4 +384,48 @@ void cb_timer(void) {
     app_dbg.num_timer++;
 
     sctimer_setCompare(sctimer_readCounter()+TIMER_PERIOD);
+}
+
+//===== uart
+
+void cb_uartTxDone(void) {
+
+    uart_clearTxInterrupts();
+
+    // prepare to send the next byte
+    app_vars.uart_lastTxByte++;
+
+    if (app_vars.uart_lastTxByte<length) {
+        uart_writeByte(app_vars.uart_txFrame[app_vars.uart_lastTxByte]);
+    } else {
+        app_vars.uart_done=1;
+    }
+}
+
+//void cb_uartTxDone(void) {
+//   app_vars.uart_lastTxByte++;
+//   if (app_vars.uart_lastTxByte<length) {
+//      uart_writeByte(stringToSend[app_vars.uart_lastTxByte]);
+//   } else {
+//      app_vars.uart_done = 1;
+//   }
+//}
+
+uint8_t cb_uartRxCb(void) {
+
+    //  uint8_t byte;
+    uart_clearRxInterrupts();
+    return 1;
+}
+
+
+void send_string(const char* str)
+{
+    strcpy(app_vars.uart_txFrame, str);
+    for (uint16_t i = length; i < sizeof(app_vars.uart_txFrame); i++)
+    app_vars.uart_txFrame[i] = 0;
+    app_vars.uart_done = 0;
+    app_vars.uart_lastTxByte = 0;
+    uart_writeByte(app_vars.uart_txFrame[app_vars.uart_lastTxByte]);
+    while(app_vars.uart_done==0);
 }
